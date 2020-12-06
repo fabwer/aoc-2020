32218cd16b53b1717f4ebc0bb411eb46f98693a9
Fabian Wernig <fabian.wernig@smarter-software.com>
2020-12-06 11:57:42 +0100

    result day 6

---
+++ src/day_6/aoc_6_utils.py
@@ -0,0 +1,6 @@
+def read_data_from_txt(filename) -> list:
+    try:
+        with open(filename) as f:
+            return f.read().split("\n\n")
+    except OSError as identifier:
+        return []
